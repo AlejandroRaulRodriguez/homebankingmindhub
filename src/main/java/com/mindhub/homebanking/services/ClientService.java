9fe19257023b36f4123366f07cd25ff8774e50e4
AlejandroRaulRodriguez <my_email>
2022-06-21 17:19:18 -0300

    cambios menores

--- src/main/java/com/mindhub/homebanking/services/ClientService.java
+++ src/main/java/com/mindhub/homebanking/services/ClientService.java
@@ -1,12 +1,11 @@
 package com.mindhub.homebanking.services;
 
 import com.mindhub.homebanking.dtos.ClientDTO;
-import com.mindhub.homebanking.models.Card;
 import com.mindhub.homebanking.models.Client;
 import org.springframework.security.core.Authentication;
 
 import java.util.List;
-import java.util.Set;
+
 
 public interface ClientService {
 
